d4c0052b23f58b4ec320c15f9f037bf2aee324a3
hadiazb <hadiazb@unal.edu.co>
2020-07-24 16:30:31 -0500

    proyecto de podcasts

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import 'isomorphic-fetch';
+import Layout from '../components/Layout';
+import ChannelGrid from '../components/ChannelGrid';
+
+export default class extends React.Component {
+	static async getInitialProps() {
+		let req = await fetch(
+			'https://api.audioboom.com/channels/recommended'
+		);
+		let { body: channels } = await req.json();
+		return { channels };
+	}
+
+	render() {
+		const { channels } = this.props;
+		return (
+			<Layout title='Podcasts'>
+				<ChannelGrid channels={channels} />
+			</Layout>
+		);
+	}
+}
